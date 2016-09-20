57883df23e2d262d78b1730bf14fac5aa21b1992
manuelpc97 <manuelpc97@unitec.edu>
2016-09-20 02:15:15 -0600

    instrucciones comentadas bajo el main

--- main.cpp
+++ main.cpp
@@ -117,7 +117,93 @@ int main(int argc, char*argv[]){
 }
 
 
+/*int size = 0;
+	int number = 0;
+	int decision = 0;
+	bool seguir = true;
+	string word = "";
+	cout << "---------------------------------------------------" << endl;
+	cout << "ingrese el tamaño de su arbol: ";
+	cin >> size;
+	btree tree(size);
+	//EL VECTOR Y EL ARREGLO VALEN PIJA
+	vector<int> lista;
+	int lista2[] = {21,33,77,98,12,6,31,44,28,4,3,99,101,200,14,1};
+
+	//ESTO ESTA EN UN WHILE PERO COMO LO VAS A ACOMODAR A NCURSES, PUES NO SE QUE PUTAS HARAS
+	while(seguir){
+		cout << "*************************************************************************" << endl;
+		cout << "1. Para Agregar. " << endl;
+		cout << "2. Para Borrar. " << endl;
+		cout << "3. Mostrar" << endl;
+		cout << "4. Cargar arbol" << endl;
+		cout << "5. Cambiar Size" << endl;
+		cout << "6 salir" << endl;
+		cin >> decision;
+
+		if(decision == 1){
+			//AQUI PEDIS EL NUMERO Y LO MANDAS A INSERTAR AL ARBOL, FIJATE QUE LA FUNCION TAMBIEN RECIBE A LA RAIZ DEL ARBOL
+			cout << "Ingrese un numero: ";
+			cin >> number;
+			tree.insert(number, tree.getRoot());
+		}else if(decision == 2){	
+			//AQUI BORRAS UN ELEMENTO
+			cout << "Ingrese un numero: ";
+			cin >> number;
+			tree.borrar(number);
+		}else if(decision == 3){
+			//AQUI MOSTRAS EL ARBOL PERO ME FALTA HACER LA FUNCION
+			treeNode** root = new treeNode*[1];
+			root[0] = tree.getRoot();
+			tree.showTree(root, 1);
+		}else if(decision == 4){
+			//AQUI CARGAS EL ARCHIVO DE TEXTO PERO ESTO FALLA
+			//loadNumbers(lista);
+			for(int i = 0; i < 16; i++){
+				lista.push_back(lista2[i]);
+			}
+			for(int i = 0; i < 16; i++){
+				tree.insert(lista[i], tree.getRoot());
+			}
+
+		}else if(decision == 5){
+			//aQUI LE CAMBIAS EL SIZE EL ARBOL EN CASO DE QUE EL USUARIO QUIERA
+			cout << "Nuevo Size: ";
+			cin >>size;
+			btree arbol(size);
+			lista = tree.getKeys();
+
+			for(int i = 0; i < lista.size(); i++){
+				arbol.insert(lista[i],arbol.getRoot());
+			}
+			tree = arbol;
+		}
+		//EN ESTA COSA MERA PIJA SE SALE DEL CICLO
+		decision == 6 ? seguir = false : seguir = true;
+	}*/
 
 
-	
+	//ESTA FUNCION ES PARA CARGAR EL ARCHIVO	
+	/*void loadNumbers(vector<int>& lista){
+	ifstream fentrada;
+	fentrada.open("numeros.txt");
+    stringstream ss;
+    string numero;
+
+    while(!fentrada.fail()){
+    	char digito;
+    	fentrada >> digito;
+    	if(!fentrada.fail()){
+    		if(digito == ','){
+    			numero = ss.str();
+    			ss.str("");
+    			int b = atoi(numero.c_str());
+    			lista.push_back(b);
+    		}else{
+    			ss << digito;
+    		}
+    	}
+    }
+    fentrada.close();
+}*/
 
