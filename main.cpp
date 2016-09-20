12aeff9e2a06463f99cb124388ec77d7e1d76f00
Alexy Cruz <xytojr@gmail.com>
2016-09-20 03:16:48 -0600

    agregar y eliminar en b y AVL (GUI)

--- main.cpp
+++ main.cpp
@@ -31,16 +31,31 @@ int main(int argc, char*argv[]){
 	int width, height;
 	char salir = '0';
 	int pruebas = 0;
-
+	string size = "";
+	char SizeTemp[15];
 
 	initscr();
 	(void)echo();
+
+	getmaxyx(curscr, height, width);
+
+	mvprintw(3,width-100,"Ingrese el tamaño de su Arbol-B: ");
+	getnstr(SizeTemp, sizeof(SizeTemp) - 1);
+	//sacar la string
+
+	for(int i = 0; i < 16; i++){
+		if(SizeTemp[i] != ' '){
+			size += SizeTemp[i];
+		}
+	}
+	int sizeArbol = atoi(size.c_str());
+	btree tree(sizeArbol);
+
 	char opcion1 = '0';
 
 
 	while(opcion1 != '3'){
-		getmaxyx(curscr, height, width);
-
+		clear();
 		init_pair(1, COLOR_BLUE, COLOR_BLACK);
 		init_pair(2, COLOR_RED, COLOR_BLACK);
 
@@ -55,53 +70,197 @@ int main(int argc, char*argv[]){
 		mvprintw(8,width-100,"Ingrese una opcion: ");
 		opcion1 = getch();
 		
-		if(opcion1 == '1'){
+		if(opcion1 == '1'){//OPCIONES AVL
 			char opcion2 = '1';
 			while(opcion2 != '0'){
 				clear();
-				string ValorIngresado = "";
-				char ValorIngresadoTemp[15];
-				int ValorAgregar;
-
 				mvprintw(3,width-100,"ARBOL Adelson-Velskii y Landis");
-				mvprintw(7, width-50, "SALIR [0]");
+				mvprintw(5,width-100,"1.- Agregar");
+				mvprintw(6,width-100,"2.- Eliminar");
+				mvprintw(7,width-100,"3.- Buscar");
+				mvprintw(8,width-100,"4.- Mostrar");
+				mvprintw(11,width-50,"SALIR[0]");
+				mvprintw(9,width-100,"Ingrese una opcion: ");
+				opcion2 = getch();
 
-				mvprintw(5,width-100,"Digite un valor para ingresar al arbol AVL: ");
-				getnstr(ValorIngresadoTemp, sizeof(ValorIngresadoTemp) - 1);
+				if(opcion2 == '1'){//Agregar AVL
+					clear();
+					string ValorIngresado = "";
+					char ValorIngresadoTemp[15];
+					int ValorAgregar;
 
-				//sacar la string
-				for(int i = 0; i < 16; i++){
-					if(ValorIngresadoTemp[i] != ' '){
-						ValorIngresado += ValorIngresadoTemp[i];
+					mvprintw(3,width-100,"ARBOL Adelson-Velskii y Landis");
+					mvprintw(7, width-50, "SALIR [0]");
+
+					mvprintw(5,width-100,"Digite un valor para ingresar al arbol AVL: ");
+					getnstr(ValorIngresadoTemp, sizeof(ValorIngresadoTemp) - 1);
+
+					//sacar la string
+					for(int i = 0; i < 16; i++){
+						if(ValorIngresadoTemp[i] != ' '){
+							ValorIngresado += ValorIngresadoTemp[i];
+						}
 					}
-				}
 
-				int ValorAgregarTemp = atoi(ValorIngresado.c_str());
-				if(ValorAgregarTemp != 0){
-					ValorAgregar = ValorAgregarTemp;
-					Arbolito.AddNode(ValorAgregar);
-					NodosTotales++;
+					int ValorAgregarTemp = atoi(ValorIngresado.c_str());
+					if(ValorAgregarTemp != 0){
+						ValorAgregar = ValorAgregarTemp;
+						Arbolito.AddNode(ValorAgregar);
+						NodosTotales++;
 
-					pruebas = ValorAgregar;
-				}else{
-					opcion2 = '0';
-				}
+						pruebas = ValorAgregar;
+					}else{
+						opcion2 = '0';
+					}
 
 
-				if(opcion2 != '0'){
-					mvprintw(6,width-100,"VALOR INGRESADO EXITOSAMENTE!");
-					refresh();
-					getch();
-				}else{
-					mvprintw(6,width-100,"Regresaras al menu principal");
-					opcion2 = '0';
-					getch();
+					if(opcion2 != '0'){
+						mvprintw(6,width-100,"VALOR INGRESADO EXITOSAMENTE!");
+						refresh();
+						getch();
+					}else{
+						mvprintw(6,width-100,"Regresaras al menu principal");
+						opcion2 = '0';
+						getch();
+					}
+				}else if(opcion2 == '2'){//eliminar AVL
+					clear();
+					string ValorIngresado = "";
+					char ValorIngresadoTemp[15];
+					int ValorBorrar;
+
+					mvprintw(3,width-100,"ARBOL Adelson-Velskii y Landis");
+					mvprintw(7, width-50, "SALIR [0]");
+
+					mvprintw(5,width-100,"Digite un valor para borrar del arbol AVL: ");
+					getnstr(ValorIngresadoTemp, sizeof(ValorIngresadoTemp) - 1);
+
+					//sacar la string
+					for(int i = 0; i < 16; i++){
+						if(ValorIngresadoTemp[i] != ' '){
+							ValorIngresado += ValorIngresadoTemp[i];
+						}
+					}
+
+					int ValorAgregarTemp = atoi(ValorIngresado.c_str());
+					if(ValorAgregarTemp != 0){
+						ValorBorrar = ValorAgregarTemp;
+						Arbolito.DeleteNode(ValorBorrar, NodosTotales, 0);
+					}else{
+						opcion2 = '0';
+					}
+
+
+					if(opcion2 != '0'){
+						mvprintw(6,width-100,"VALOR INGRESADO EXITOSAMENTE!");
+						refresh();
+						getch();
+					}else{
+						mvprintw(6,width-100,"Regresaras al menu principal");
+						opcion2 = '0';
+						getch();
+					}
+				}else if(opcion2 == '3'){//buscar AVL
+
+				}else if(opcion2 == '4'){//MOSTRAR AVL
+
 				}
 			}
-		}else if(opcion1 == '2'){
+		}else if(opcion1 == '2'){//OPCIONES ARBOL-B
 			char opcion3 = '1';
 			while(opcion3 != '0'){
+				clear();
 				mvprintw(3,width-100,"ARBOL-B");
+				mvprintw(5,width-100,"1.- Agregar");
+				mvprintw(6,width-100,"2.- Eliminar");
+				mvprintw(7,width-100,"3.- Cargar");
+				mvprintw(8,width-100,"4.- Mostrar");
+				mvprintw(9,width-100,"5.- Cambiar size");
+				mvprintw(12,width-50,"SALIR[0]");
+				mvprintw(10,width-100,"Ingrese una opcion: ");
+				opcion3 = getch();
+
+				if(opcion3 == '1'){//AGREGAR ARBOL-B
+					clear();
+					string ValorIngresado = "";
+					char ValorIngresadoTemp[15];
+					int ValorAgregar;
+
+					mvprintw(3,width-100,"ARBOL-B");
+					mvprintw(7, width-50, "SALIR [0]");
+
+					mvprintw(5,width-100,"Digite un valor para ingresar al Arbol-B: ");
+					getnstr(ValorIngresadoTemp, sizeof(ValorIngresadoTemp) - 1);
+
+					//sacar la string
+					for(int i = 0; i < 16; i++){
+						if(ValorIngresadoTemp[i] != ' '){
+							ValorIngresado += ValorIngresadoTemp[i];
+						}
+					}
+
+					int ValorAgregarTemp = atoi(ValorIngresado.c_str());
+					if(ValorAgregarTemp != 0){
+						ValorAgregar = ValorAgregarTemp;
+						//tree.insert(ValorAgregar, tree.getRoot());
+					}else{
+						opcion3 = '0';
+					}
+
+
+					if(opcion3 != '0'){
+						mvprintw(6,width-100,"VALOR INGRESADO EXITOSAMENTE!");
+						refresh();
+						getch();
+					}else{
+						mvprintw(6,width-100,"Regresaras al menu principal");
+						opcion3 = '0';
+						getch();
+					}
+				}else if(opcion3 == '2'){//ELIMINAR ARBOL-B
+					clear();
+					string ValorIngresado = "";
+					char ValorIngresadoTemp[15];
+					int ValorBorrar;
+
+					mvprintw(3,width-100,"ARBOL-B");
+					mvprintw(7, width-50, "SALIR [0]");
+
+					mvprintw(5,width-100,"Digite un valor para borrar del Arbol-B: ");
+					getnstr(ValorIngresadoTemp, sizeof(ValorIngresadoTemp) - 1);
+
+					//sacar la string
+					for(int i = 0; i < 16; i++){
+						if(ValorIngresadoTemp[i] != ' '){
+							ValorIngresado += ValorIngresadoTemp[i];
+						}
+					}
+
+					int ValorAgregarTemp = atoi(ValorIngresado.c_str());
+					if(ValorAgregarTemp != 0){
+						ValorBorrar = ValorAgregarTemp;
+						tree.borrar(ValorBorrar);
+					}else{
+						opcion3 = '0';
+					}
+
+
+					if(opcion3 != '0'){
+						mvprintw(6,width-100,"VALOR INGRESADO EXITOSAMENTE!");
+						refresh();
+						getch();
+					}else{
+						mvprintw(6,width-100,"Regresaras al menu principal");
+						opcion3 = '0';
+						getch();
+					}
+				}else if(opcion3 == '3'){//CARGAR ARBOL-B
+
+				}else if(opcion3 == '4'){//MOSTRAR ARBOL-B
+
+				}else if(opcion3 == '5'){//CAMBIAR SIZE ARBOL-B
+
+				}
 			}
 		}
 		clear();
